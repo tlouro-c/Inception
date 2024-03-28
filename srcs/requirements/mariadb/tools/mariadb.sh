b0aa545075bcfc8e288458604a153cfd1c611a20
tlouro-c <tomascorreia.lc@gmail.com>
2024-03-28 17:57:43 +0000

    Feature: Finished mariadb.sh script

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,10 +1,24 @@
 #! /bin/bash
 
-wordpress_db_dir="/var/lib/mysql/wordpress_db"
+wordpress_db_dir="/var/lib/mysql/$DB"
 
-if [ ! -d wordpress_db_dir ] then # If the wordpress_db directory doesn't exist, then create the database
-	mysql_secure_installation
+if [ ! -d "$wordpress_db_dir" ]; then # If the wordpress_db directory doesn't exist, then create the database
+	mysql_secure_installation <<EOF
 
+y
+$MYSQL_ROOT_PASSWORD
+$MYSQL_ROOT_PASSWORD
+y
+y
+y
+y
+EOF
 
-CREATE DATABASE wordpress_db;
-
+	mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+CREATE DATABASE $DB;
+CREATE USER '$MYSQL_MANAGER'@'localhost' IDENTIFIED BY '$MYSQL_MANAGER_PASSWORD';
+GRANT ALL PRIVILEGES ON "$DB".* TO '$MYSQL_MANAGER'@'localhost';
+FLUSH PRIVILEGES;
+EXIT;
+EOF
+fi
