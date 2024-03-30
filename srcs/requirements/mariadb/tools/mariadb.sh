359bc7b3ef9bc705aca4c433c4e1218d1c3e2adc
tlouro-c <tomascorreia.lc@gmail.com>
2024-03-30 20:23:30 +0000

    Fix: mariadb.sh script fixed

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -3,25 +3,33 @@
 wordpress_db_dir="/var/lib/mysql/$DB"
 
 if [ ! -d "$wordpress_db_dir" ]; then # If the wordpress_db directory doesn't exist, then create the database
-	mysql_secure_installation <<EOF
 
-y
-$MYSQL_ROOT_PASSWORD
-$MYSQL_ROOT_PASSWORD
-y
-y
-y
-y
-EOF
+service mariadb start
 
-	mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<EOF
+mysql -u root <<_EOF_ # mysql_secure_installation replacement
+SET PASSWORD FOR 'root'@'localhost' = PASSWORD('$MYSQL_ROOT_PASSWORD');
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+DROP DATABASE IF EXISTS test;
+DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+FLUSH PRIVILEGES;
+exit
+_EOF_
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" <<_EOF_
 CREATE DATABASE $DB;
 CREATE USER '$MYSQL_MANAGER'@'localhost' IDENTIFIED BY '$MYSQL_MANAGER_PASSWORD';
-GRANT ALL PRIVILEGES ON "$DB".* TO '$MYSQL_MANAGER'@'localhost';
+GRANT ALL PRIVILEGES ON $DB.* TO '$MYSQL_MANAGER'@'localhost';
 FLUSH PRIVILEGES;
-EXIT;
-EOF
-echo test
+exit
+_EOF_
+
+sleep 1 # This sleep is key, and for my own understanding, \
+		# when mariadb is started, it needs some time to create the socket file
+		# so when it's stopped, the socket exists and there's no error.
+
+service mariadb stop
+
 fi
 
 mysqld_safe
+
