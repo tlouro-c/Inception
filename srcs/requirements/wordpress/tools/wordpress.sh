69aba4e2764147992d2577abcd099041c9da2702
tlouro-c <tomascorreia.lc@gmail.com>
2024-03-31 17:06:08 +0000

    Fixed wordpress.sh script, added command to run php-fpm on foreground to mantain the container running listening for requests

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -2,21 +2,17 @@
 
 cd /var/www/html/
 
-if [ ! -f "wp-config.php" ]; then
+if [ ! -f wp-config.php ]; then
 	wget https://wordpress.org/latest.tar.gz
 	tar xfz latest.tar.gz
 	rm -f latest.tar.gz
-
-	curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp
 
 	cp wordpress/wp-config-sample.php wordpress/wp-config.php
 
 	sed -i "s/database_name_here/"$DB"/g" wordpress/wp-config.php
 	sed -i "s/username_here/"$MYSQL_MANAGER"/g" wordpress/wp-config.php
 	sed -i "s/password_here/"$MYSQL_MANAGER_PASSWORD"/g" wordpress/wp-config.php
-	sed -i "s/localhost/mariadb/g" wordpress/wp-config.php
+	sed -i "s/localhost/"$DB_HOSTNAME"/g" wordpress/wp-config.php
 
 	rm wordpress/wp-config-sample.php
 	
@@ -27,7 +23,7 @@ if [ ! -f "wp-config.php" ]; then
 	--admin_user=$WP_ADMIN \
 	--admin_password=$WP_ADMIN_PASSWORD \
 	--admin_email=$WP_ADMIN_EMAIL --skip-email
-	wp user create --allow-root $WP_USER --user_pass=$WP_USER_PASSWORD
+	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASSWORD --allow-root;
 fi
 
-
+php-fpm7.4 -F
