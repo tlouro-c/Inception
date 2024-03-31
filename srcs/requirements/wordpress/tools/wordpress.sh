882d9ea1338a609d6c7a9c561f854903fff6f85e
tlouro-c <tomascorreia.lc@gmail.com>
2024-03-31 00:49:06 +0000

    Fixed: wordpress script and hostnames for users in databases

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -1,4 +1,6 @@
-#! /bin/bash
+#!/bin/bash
+
+cd /var/www/html/
 
 if [ ! -f "wp-config.php" ]; then
 	wget https://wordpress.org/latest.tar.gz
@@ -14,14 +16,18 @@ if [ ! -f "wp-config.php" ]; then
 	sed -i "s/database_name_here/"$DB"/g" wordpress/wp-config.php
 	sed -i "s/username_here/"$MYSQL_MANAGER"/g" wordpress/wp-config.php
 	sed -i "s/password_here/"$MYSQL_MANAGER_PASSWORD"/g" wordpress/wp-config.php
-	sed -i "s/localhost/mariadb/g" wordpress/wp-config.php
+	sed -i "s/localhost/0a2d206973e9/g" wordpress/wp-config.php
+
+	rm wordpress/wp-config-sample.php
 	
 	mv wordpress/* .
 	rm -rf wordpress
 
-	wp core install --allow-root --url="$DOMAIN" --title=TomsWebsite \
-	--admin_user="$WP_ADMIN" --admin_password="$WP_ADMIN_PASSWORD"
-	wp user create --allow-root "$WP_USER" --user_pass="$WP_USER_PASSWORD"
+	wp core install --allow-root --url=$DOMAIN --title=TomsWebsite \
+	--admin_user=$WP_ADMIN \
+	--admin_password=$WP_ADMIN_PASSWORD \
+	--admin_email=$WP_ADMIN_EMAIL --skip-email
+	wp user create --allow-root $WP_USER --user_pass=$WP_USER_PASSWORD
 fi
 
 
