728f5597b952c16b09dd7bac88f77bdc1d61e324
tlouro-c <tomascorreia.lc@gmail.com>
2024-04-03 16:09:20 +0000

    Fix: Wordpress Installation URL

--- srcs/requirements/wordpress/tools/wordpress.sh
+++ srcs/requirements/wordpress/tools/wordpress.sh
@@ -19,7 +19,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	mv wordpress/* .
 	rm -rf wordpress
 
-	wp core install --allow-root --url=$DOMAIN --title=TomsWebsite \
+	wp core install --allow-root --url=tlouro-c.42.fr --title=TomsWebsite \
 	--admin_user=$WP_ADMIN \
 	--admin_password=$WP_ADMIN_PASSWORD \
 	--admin_email=$WP_ADMIN_EMAIL --skip-email
